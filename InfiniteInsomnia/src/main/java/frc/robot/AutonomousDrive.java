917227f57753d287dd922165f0a527257b91adcb
latka-krystof <74324358+latka-krystof@users.noreply.github.com>
2021-01-09 16:41:01 +0100

    edit autoDrive()

--- InfiniteInsomnia/src/main/java/frc/robot/AutonomousDrive.java
+++ InfiniteInsomnia/src/main/java/frc/robot/AutonomousDrive.java
@@ -19,23 +19,23 @@ public class AutonomousDrive {
 
     public void autoDrive() {
         //Jede doleva 0.5 sekundy (a pak rovně, aby se mohl otočit)
-        if (timer.get() >= 0 && timer.get() < 0.5) {
+        if (timer.get() < 0.5) {
             robotMap.drive.driveCartesian(0, 0.75, 0);
         }
         //Jede rovně 0.5 sekundy
-        if (timer.get() >= 0.5 && timer.get() < 1) {
+        else if (timer.get() < 1) {
             robotMap.drive.driveCartesian(0.75, 0, 0);
         }
         //Otočka o 45°, aby objel Brick
-        if (timer.get() >= 1 && timer.get() < 1.5) {
+        else if (timer.get() < 1.5) {
             robotMap.drive.driveCartesian(0, 0, 0.75);
         }
         //Rovně před Flask
-        if (timer.get() >= 1.5 && timer.get() < 2) {
+        else if (timer.get() < 2) {
             robotMap.drive.driveCartesian(0.75, 0, 0);
         }
         //Otočka o 135° na střelbu
-        if (timer.get() >= 2) {
+        else if (timer.get() < 2.5) {
             robotMap.drive.driveCartesian(0, 0, 0.75);
         }
      }
