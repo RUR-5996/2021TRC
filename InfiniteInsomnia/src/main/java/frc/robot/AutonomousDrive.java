f02aad68915515a34c81295bbc952d2f7f7e2642
latka-krystof <74324358+latka-krystof@users.noreply.github.com>
2021-02-03 20:44:14 +0100

    Update AutonomousDrive.java

--- InfiniteInsomnia/src/main/java/frc/robot/AutonomousDrive.java
+++ InfiniteInsomnia/src/main/java/frc/robot/AutonomousDrive.java
@@ -13,6 +13,7 @@ public class AutonomousDrive {
 
     public void autoDrive() {
 
+        //drive approximately to the middle of the field
         while ((RobotMap.ultrasonic.getVoltage() / Constants.voltsPerCm) > 250)
         {
             robotMap.drive.driveCartesian(0, 1.0, 0);
@@ -20,6 +21,7 @@ public class AutonomousDrive {
 
         RobotMap.ultrasonic.resetAccumulator();
 
+        //turn 35 degrees, counter-clockwise towards the flask
         while (RobotMap.gyro.getAngle() > -35)
         {
             robotMap.drive.driveCartesian(0.0, 0.0, -1.0);
@@ -27,11 +29,13 @@ public class AutonomousDrive {
         
         RobotMap.gyro.reset();
 
+        //drive until the robot is 90 cm from the flask
         while (RobotMap.ultrasonic.getVoltage() / Constants.voltsPerCm > 90)
         {
             robotMap.drive.driveCartesian(0.0, 1.0, 0.0);
         }
 
+        //turn 35 degrees clock-wise so that the robot faces the flask 
         while (RobotMap.gyro.getAngle() < 35)
         {
             robotMap.drive.driveCartesian(0.0, 0.0, 1.0);
