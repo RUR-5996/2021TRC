74b510734408bdf278872940f10f5ac9612b38b8
Juli Šejnohová <76658407+julisejnohova@users.noreply.github.com>
2021-02-23 23:39:28 +0100

    Update Constants.java

--- InfiniteInsomnia/src/main/java/frc/robot/Constants.java
+++ InfiniteInsomnia/src/main/java/frc/robot/Constants.java
@@ -5,16 +5,16 @@ public class Constants {
     //controllers
     public static final int CONTROLLER = 1;
     public static final double DEADZONE = 0.2;
-    public static final int buttonA = 1;
-    public static final int buttonB = 2;
-    public static final int buttonX = 3;
-    public static final int buttonY = 4;
-    public static final int leftBumper = 5;
-    public static final int rightBumper = 6;
-    public static final int stop = 7;
-    public static final int start = 8;
-    public static final int leftJoystickButton = 9;
-    public static final int rightJoystickButton = 10;
+    public static final int ButtonA = 1;
+    public static final int ButtonB = 2;
+    public static final int ButtonX = 3;
+    public static final int ButtonY = 4;
+    public static final int LeftBumper = 5;
+    public static final int RightBumper = 6;
+    public static final int Stop = 7;
+    public static final int Start = 8;
+    public static final int LeftJoystickButton = 9;
+    public static final int RightJoystickButton = 10;
 
 }
 
